58d363c7288c09b943ae7e45933cafe4b8cf5706
K A V VINAY KARTHIK <127183670+kavvinaykarthik@users.noreply.github.com>
2025-03-09 12:19:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-wget -O yolov8n.pt https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8n.pt
+if [ ! -f "yolov8n.pt" ]; then
+    wget -O yolov8n.pt https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8n.pt
+fi
 uvicorn app:app --host 0.0.0.0 --port 8000 --reload
