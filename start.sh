1c302554762434a1348725d4505ae973906fedd7
K A V VINAY KARTHIK <127183670+kavvinaykarthik@users.noreply.github.com>
2025-03-09 12:13:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
-uvicorn main:app --host 0.0.0.0 --port $PORT
+#!/bin/bash
+pip install -r requirements.txt
+wget -O yolov8n.pt https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8n.pt
+uvicorn app:app --host 0.0.0.0 --port 8000 --reload
