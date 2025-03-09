0a473269076334a6fef48adf5bece4ca0b72dffd
K A V VINAY KARTHIK <127183670+kavvinaykarthik@users.noreply.github.com>
2025-03-09 13:06:50 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ ! -f "yolov8n.pt" ]; then
-    wget -O yolov8n.pt https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8n.pt
-fi
-uvicorn app:app --host 0.0.0.0 --port 8000 --reload
